a8ab4e7b2bcd4e8bc0f64be049e7b99e4cc48646
JKal24 <jkalirai2499@gmail.com>
2023-09-14 20:15:41 -0400

    Creating app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,101 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: appBar(),
+      body: Column (children: [
+        Container(
+          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.all(15),
+              hintText: 'Search...',
+              hintStyle: const TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg')
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(child: 
+                  Row (
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset('assets/icons/Filter.svg')
+                      )
+                    ]
+                  )
+                )
+              ) ,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              )
+            ),
+          )
+        )
+      ],)
     );
   }
+}
+
+AppBar appBar() {
+  return AppBar(
+    title: const Text(
+      "Weather",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+    ),
+    backgroundColor: Colors.white,
+    elevation: 0.0,
+    centerTitle: true,
+    leading: Container(
+      alignment: Alignment.center,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xffF7F8F8),
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+    ),
+    actions: [
+      GestureDetector(
+        onTap: () {  },
+        child: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(10),
+        width: 40,
+        decoration: BoxDecoration(
+          color: const Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: SvgPicture.asset('assets/icons/dots.svg'),
+        )
+      )
+    ],
+  );
 }
